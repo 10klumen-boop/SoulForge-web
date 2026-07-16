a8256cf5bf853b870c50f9888fab7735cf7233e4
SoulForge Export <soulforge-export@local>
2026-07-17 02:26:23 +0300

    Drop temporary commit helper from tree

--- _do_commit.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d D:\SoulForge-web
-git add -A
-git -c user.email=soulforge-export@local -c user.name="SoulForge Export" commit -m "Remove helper batch; keep push instructions"
-git log -2 --oneline
-git status -sb
-del "%~f0"
